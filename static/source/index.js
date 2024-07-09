39272c3b6d8fcee2dedf35627c99504a4cb0ee38
Tim Ebbeke <Tim06TR@gmail.com>
2024-07-09 02:23:03 +0200

    Improved code theming code location.

--- static/source/index.js
+++ static/source/index.js
@@ -1,6 +1,15 @@
 // import './livereload.ts';
 import { nanoid } from "nanoid";
 
+import "@ui5/webcomponents/dist/Assets.js";
+import "@ui5/webcomponents-fiori/dist/Assets.js";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+import { setThemeRoot } from "@ui5/webcomponents-base/dist/config/ThemeRoot.js";
+
 (() => {
     globalThis.nanoid = nanoid;
+
+    setTheme("dark");
+    // app.example is intentional, because of windows dns timeouts
+    setThemeRoot("nui://app.example/");
 })();
