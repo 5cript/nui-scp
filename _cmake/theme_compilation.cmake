2d4ee27d9dac61f6e1dbaac1c5928181535f219b
Tim Ebbeke <Tim06TR@gmail.com>
2024-07-13 01:02:40 +0200

    Removed symlink for themes on windows.

--- _cmake/theme_compilation.cmake
+++ _cmake/theme_compilation.cmake
@@ -11,9 +11,15 @@ foreach(THEME_NAME ${THEME_NAMES})
     list(APPEND THEME_DEPENDS "${CMAKE_SOURCE_DIR}/themes/${THEME_NAME}/theme.less")
 endforeach()
 
+set(SYMLINK_COMMAND "")
+if(CMAKE_HOST_WIN32)
+else()
+    set(SYMLINK_COMMAND COMMAND ${CMAKE_COMMAND} -E create_symlink "${CMAKE_CURRENT_BINARY_DIR}/node_modules" "${CMAKE_SOURCE_DIR}/themes/node_modules")
+endif()
+
 add_custom_command(
     OUTPUT ${THEME_OUTPUTS}
-    COMMAND ${CMAKE_COMMAND} -E create_symlink "${CMAKE_CURRENT_BINARY_DIR}/node_modules" "${CMAKE_SOURCE_DIR}/themes/node_modules"
+    ${SYMLINK_COMMAND}
     COMMAND ${NUI_NODE} "${CMAKE_SOURCE_DIR}/themes/compile.mjs" "${THEME_OUTPUT_DIRECTORY}"
     DEPENDS ${THEME_DEPENDS}
     WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
