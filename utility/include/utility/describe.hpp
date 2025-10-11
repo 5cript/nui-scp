a212f717fa77b696a0f175a007482c45b46fa4d6
Tim Ebbeke <Tim06TR@gmail.com>
2025-10-11 22:51:51 +0200

    Added include.

--- utility/include/utility/describe.hpp
+++ utility/include/utility/describe.hpp
@@ -1,4 +1,5 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #include <boost/describe.hpp>
+#include <boost/mp11/algorithm.hpp>
 #pragma clang diagnostic pop
